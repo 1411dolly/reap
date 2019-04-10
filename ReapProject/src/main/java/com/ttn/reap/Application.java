5b83054b234b259fc81eef691fd3855f23bababc
Dharmendra <dharmendra@tothenew.com>
2019-04-10 17:39:55 +0530

    merge wednessday

--- ReapProject/src/main/java/com/ttn/reap/Application.java
+++ ReapProject/src/main/java/com/ttn/reap/Application.java
@@ -2,6 +2,7 @@ package com.ttn.reap;
 
 import com.ttn.reap.property.FileStorageProperties;
 import com.ttn.reap.repository.UserRepository;
+import com.ttn.reap.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -23,6 +24,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
         System.out.println("welcome to reap!!!");
-
     }
 }
